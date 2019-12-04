f11be637c2b4ff7855476371c15fd0c6174f585c
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-12-04 03:11:59 +0000

    Added mailer function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,40 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:destroy, :edit]
+  # before_action :set_user, only: [:destroy, :edit]
 
   def index
     @users = User.all
   end
 
-  def destroy
-    @user.destroy
-    redirect_to users_path, notice: 'ユーザー削除しました'
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to users_path, notice:"ユーザーを編集しました！"
-    else
-      render :edit
-    end
-  end
-
-
-
-
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
-  end
+  # def destroy
+  #   @user.destroy
+  #   redirect_to users_path, notice: 'ユーザー削除しました'
+  # end
+  #
+  # def edit
+  # end
+  #
+  # def update
+  #   if @user.update(user_params)
+  #     redirect_to users_path, notice:"ユーザーを編集しました！"
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  #
+  #
+  #
+  #
+  # private
+  #
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
+  #
+  # def user_params
+  #   params.require(:user).permit(:name, :email, :password,
+  #                                   :password_confirmation)
+  # end
 end
 
   #
