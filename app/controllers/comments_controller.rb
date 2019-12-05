2d6d99c64362760cce265fea6b24aeb3841e204d
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-12-05 02:17:25 +0000

    IN trying to add delete function to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end 
+
   def create
     @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.build(comment_params)
@@ -6,9 +10,21 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.js { render :index }
-      else 
+      else
         format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした。。'}
       end
+    end
+  end
+
+  def destroy
+  binding.pry
+  @comment = @blog.comment.find(params[:id])
+  respond_to do |format|
+    if current_user.id == @comment.user_id
+      @comment.destroy
+      format.js { render :index }
+    else
+      format.html { redirect_to blog_path(@blog), notice: '削除できませんでした。。'}
     end
   end
 
