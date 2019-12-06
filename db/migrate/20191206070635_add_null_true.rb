1c6b11f017a250250e33a1ec91a95bb36a58ec42
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-12-06 12:08:03 +0000

    Edited READ ME

--- db/migrate/20191206070635_add_null_true.rb
+++
@@ -1,8 +0,0 @@
-class AddNullTrue < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :blogs, :cut_off_date, true
-    change_column_null :blogs, :status, true
-    change_column_null :blogs, :priority, true
-  end
-
-  end
