059fbed1ea63f049d60b8b0c68176de7dd7bcaf1
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-12-20 11:42:50 +0000

    update

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,5 @@
 class Food < ApplicationRecord
   mount_uploader :picture, ImageUploader
-
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length:{in:1..10}
